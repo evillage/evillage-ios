d1e15d790bfe20b04b2c07d5957e694c8cda76e2
tpadalko <tpadalko@worth.systems>
2019-11-10 18:47:03 +0100

    Initial Commit

---
+++ ClangNotifications/ClangNotifications.h
@@ -0,0 +1,19 @@
+//
+//  ClangNotifications.h
+//  ClangNotifications
+//
+//  Created by Tetiana Padalko on 10/11/2019.
+//  Copyright © 2019 Worth Internet Systems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ClangNotifications.
+FOUNDATION_EXPORT double ClangNotificationsVersionNumber;
+
+//! Project version string for ClangNotifications.
+FOUNDATION_EXPORT const unsigned char ClangNotificationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClangNotifications/PublicHeader.h>
+
+
