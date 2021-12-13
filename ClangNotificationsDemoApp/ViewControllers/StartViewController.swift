7a3763e666638bcf59100fb58fe1e06fd9383a6e
Livecast71 <35631641+Livecast71@users.noreply.github.com>
2021-12-13 11:06:19 +0100

    registerAccountWithProperties
    registerAccountWithProperties added to RegisterAccount, updateProperties and logEvent

--- ClangNotificationsDemoApp/ViewControllers/StartViewController.swift
+++ ClangNotificationsDemoApp/ViewControllers/StartViewController.swift
@@ -18,7 +18,7 @@ class StartViewController: UIViewController {
     
         let htmlString = "<!DOCTYPE html><html><head><meta name=\"viewport\"content=\"width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no\"/><link href=\"https://fonts.googleapis.com/css?family=PT+Sans:400,700\" rel=\"stylesheet\"><style>table {width:100%;font-size:17px;font-family:PT Sans,Helvetica,Arial,sans-serif;color:#003b5a;line-height:24px;}.title{font-size:20px;}.button{text-align:left;color:#00a4a7;font-weight:bold;text-decoration:none;}</style></head><body style=\"background-color:transparent;\"><table><tr><td><table><tr><td width=\"40px;\"><img src=\"https://i.ibb.co/ggbFyTT/Asset-360-10-R.png\" width=\"35px\"></td><td class=\"title\"><b>Particuliere rekening openen</b></td></tr></table></td></tr><tr><td>Naast zakelijk ook privé bij ons bankieren? Druk in de Knab App op het plusje rechtsboven en kies voor ‘betaalrekening’. Stap nu over en ontvang € 50!</td><tr><td class=\"button\"></td></tr></tr></table></body></html>"
         
-        //ClangFunctions().buildTheTickets(parent: self, toadd: htmlString)
+        ClangFunctions().buildTheTickets(parent: self, toAdd: htmlString)
     }
     
     @IBAction func changeit(_ sender: UISlider) {
