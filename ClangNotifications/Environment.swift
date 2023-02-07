9ae3c85a79c62837cf3f5644839540bba3f023f8
Livecast71 <35631641+Livecast71@users.noreply.github.com>
2023-02-07 15:30:42 +0100

    Environment adjusted
    authorizationToken made public, integrationId made public

--- ClangNotifications/Environment.swift
+++ ClangNotifications/Environment.swift
@@ -11,46 +11,13 @@ import Foundation
 /// Contains Key/Values to get all the information we need from the info.plist
 public enum Environment {
 
-  // MARK: - Keys
+    // MARK: - values
 
-  /// The Info.plist keys we want to retrieve data from
-  enum PlistKeys {
-    /// The token we use in the authorization header of the APi calls
-    static let authorizationToken = "AUTHORIZATION_TOKEN"
-    /// The id we use in the body of the API calls
-    static let integrationId = "INTEGRATION_ID"
-  }
+    /// Ready  set url
+    static let baseURL: URL = URL(string: "https://api.clang.cloud")!
+    /// Public authorizationToken provided.  If there is no Authorization token set this code will crash
+    public static let authorizationToken: String = ""
+    /// Public integrationId provided. If there is no integration id set this code will crash
+    public static let integrationId: String = ""
 
-  // MARK: - Plist
-
-  /// Get the info dictioanry of the integrator app. If none is found this code will crash
-  private static let infoDictionary: [String: Any] = {
-    guard let dict = Bundle.main.infoDictionary else {
-      fatalError("Plist file not found")
-    }
-    return dict
-  }()
-
-  // MARK: - Plist values
-
-  /// Get the root url from the info.plist found in the integrator's app. If there is no root url set this code will crash
-  static let baseURL: URL = URL(string: "https://api.clang.cloud")!
-
-  /// Get the authorization token from the info.plist found in the integrator's app. If there is no Authorization token set this code will crash
-  static let authorizationToken: String = {
-    guard let authToken = Environment.infoDictionary[PlistKeys.authorizationToken] as? String else {
-      fatalError("Authorization token was not set in info.plist")
-    }
-
-    return authToken
-  }()
-
-  /// Get the integration id from the info.plist found in the integrato's app. If there is no integration id set this code will crash
-  static let integrationId: String = {
-    guard let integrationId = Environment.infoDictionary[PlistKeys.integrationId] as? String else {
-      fatalError("Integration ID was not set in info.plist")
-    }
-
-    return integrationId
-  }()
 }
